930567a1c1a7b98ea5962f038c26144fe654506a
david-rosval <20b3010135@untels.edu.pe>
2024-09-20 09:24:51 -0500

    crear la visión del proyecto listo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,50 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ingeniería de Software';
+export const SITE_DESCRIPTION = 'Blog para la presentación de avances del proyecto de Ingeniería de Software';
+
+export const AVANCES = [
+  {
+    slug: 'crear-la-vision-del-proyecto',
+    data: {
+      heroImage: '',
+      title: 'Crear la visión del proyecto',
+    }
+  },
+  {
+    slug: 'identificar-al-scrum-master-y-stakeholders',
+    data: {
+      heroImage: '',
+      title: 'Identificar al Scrum Master y stakeholders',
+    }
+  },
+  {
+    slug: 'formar-al-equipo-scrum',
+    data: {
+      heroImage: '',
+      title: 'Formar al equipo Scrum',
+    }
+  },
+  {
+    slug: 'desarrollar-epicas',
+    data: {
+      heroImage: '',
+      title: 'Desarrollar épicas',
+    }
+  },
+  {
+    slug: 'crear-el-backlog-priorizado-del-producto',
+    data: {
+      heroImage: '',
+      title: 'Crear el backlog priorizado del producto',
+    }
+  },
+  {
+    slug: 'realizar-la-planificacion-del-lanzamiento',
+    data: {
+      heroImage: '',
+      title: 'Realizar la planificación del lanzamiento',
+    }
+  },
+]
